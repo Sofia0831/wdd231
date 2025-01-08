dbc1f4a6f96947e83952a4e2807e7d95c3f0943c
sofiapantas <sofiasarabia0831@gmail.com>
2025-01-09 00:33:25 +0800

    media-query

--- scripts/getdate.js
+++
@@ -1,8 +0,0 @@
-
-const currentYear = new Date().getFullYear();
-
-const lastModifiedDate = document.lastModified;
-
-const footerParagraphs = document.querySelector("footer").querySelectorAll("p");
-footerParagraphs[0].textContent = `©️${currentYear} 🎭 Sofia Florylle S. Pantas 🎭 Philippines`;
-footerParagraphs[1].textContent = `Last modified: ${lastModifiedDate}`;
