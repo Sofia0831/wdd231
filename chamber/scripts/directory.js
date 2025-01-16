e4ab07a1f343ba78420751b51b74bf3d4b682e7b
sofiapantas <sofiasarabia0831@gmail.com>
2025-01-16 23:40:17 +0800

    skeleton

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,15 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('#animate');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+const currentYear = new Date().getFullYear();
+
+const lastModifiedDate = document.lastModified;
+
+const footerParagraphs = document.querySelector("footer").querySelectorAll("p");
+footerParagraphs[2].textContent = `©${currentYear} Biliran Chamber of Commerce`;
+footerParagraphs[3].textContent = `Last modified: ${lastModifiedDate}`;
