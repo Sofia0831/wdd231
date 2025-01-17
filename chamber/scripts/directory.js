94f1e17170d89cab0109146344215727dacb2c6b
sofiapantas <sofiasarabia0831@gmail.com>
2025-01-17 15:54:08 +0800

    weather

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -6,6 +6,62 @@ hamButton.addEventListener('click', () => {
 	hamButton.classList.toggle('open');
 });
 
+const apiKey = '884d02f5eb1903be4f0c638b89335b31';
+const city = 'Biliran';
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+
+fetch(apiUrl)
+	.then(response => response.json())
+	.then (data => {
+		const weatherInfo = document.getElementById('weather-info');
+		const weatherIcons = {
+			'Clear': 'images/clear.svg',
+			'Clouds': 'images/clouds.svg',
+			'Rain': 'images/rain.svg',
+			'Drizzle': 'images/drizzle.svg',
+			'Thunderstorm': 'images/thunderstorm.svg'
+		};
+		const iconUrl = weatherIcons[data.weather[0].main] || 'images/uknown.svg';
+
+		weatherInfo.innerHTML = `
+		<img src="${iconUrl}" alt="${data.weather[0].description}" width="80">
+		<p>Temperature: ${data.main.temp}°C <br>
+		Description: ${data.weather[0].description} <br>
+		Humidity: ${data.main.humidity}%</p>
+		`;
+	})
+	.catch(error => {
+	    console.error('Error fetching weather data:', error);
+	    const weatherInfo = document.getElementById('weather-info');
+	    weatherInfo.innerHTML = 'Error fetching weather data.';
+	});
+
+
+	fetch(forecastUrl)
+    .then(response => response.json())
+    .then(data => {
+      const forecastInfo = document.getElementById('forecast-info');
+
+      const forecastData = data.list.filter(item => {
+        const date = new Date(item.dt_txt);
+        return date.getHours() === 12;
+      }).slice(0, 3);
+
+      forecastData.forEach(forecast => {
+        const date = new Date(forecast.dt_txt);
+        const dayofWeek = date.toLocaleDateString('en-US', { weekday: 'short' });
+        const temperature = forecast.main.temp;
+
+        forecastInfo.innerHTML += `<p>${dayofWeek}: <strong>${temperature}°C </strong></p>`;
+      });
+    })
+    .catch(error => {
+      console.error('Error fetching weather data:', error);
+      const forecastInfo = document.getElementById('forecast-info-2');
+      forecastInfo.innerHTML = 'Error fetching weather data.';
+    });
+
 const currentYear = new Date().getFullYear();
 
 const lastModifiedDate = document.lastModified;
